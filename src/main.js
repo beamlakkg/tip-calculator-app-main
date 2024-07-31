3dbdc45c31732517ce468260496df30c091126e6
beamlakkg <beamlakkg@gmail.com>
2024-07-31 13:47:39 +0800

    inital commit

--- src/main.js
+++ src/main.js
@@ -3,11 +3,23 @@ const billInput = document.getElementById('bill');
 const tipButtons = document.querySelectorAll('.btn');
 const customTipInput = document.getElementById('customtip');
 const peopleInput = document.getElementById('people');
-const billInput = document.getElementById('bill');
-const billInput = document.getElementById('bill');
-const billInput = document.getElementById('bill');
-const billInput = document.getElementById('bill');
+const AmountDisplay = document.getElementById('tip-amount');
+const totalAmountDisplay = document.getElementById('total-amount');
+const resetButton = document.getElementById('reset');
+const errorMessage = document.getElementById('error-message');
 
+let billValue=0.0;
+let tipValue= 0.0;
+let peopleValue= 1;
+
+function calculateTip(){
+    if (peopleValue >=1){
+        let tipAmount= (billValue*tipValue)/100/peopleValue;
+        let totalAmount=(biillvalue/peopleValue)+tipAmount;
+        tipAmountDisplay.textContent = $${tipAmount.toFixed(2)};
+        totalAmountDisplay.textContent= $${totalAmount.toFixed(2)};
+    }
+}
 })
 
 
